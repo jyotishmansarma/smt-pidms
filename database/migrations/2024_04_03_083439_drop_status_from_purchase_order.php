5cb62912561da20215ee0aeff63a71e58c48c599
manoj199x <manojsaharia45@gmail.com>
2024-04-03 14:21:43 +0530

    migration added for status foreign

---
+++ database/migrations/2024_04_03_083439_drop_status_from_purchase_order.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class DropStatusFromPurchaseOrder extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('purchase_orders', function (Blueprint $table) {
+            
+            $table->dropColumn('is_verified','is_completed');
+            $table->unsignedBigInteger('status')->after('order_grand_total')->default(1);
+            $table->foreign('status')->references('id')->on('statuses');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('purchase_orders', function (Blueprint $table) {
+            //
+        });
+    }
+}
