1a54c3b93cd8b870ee1e887a89c89960a20599f5
saranga pani sarma <sarangapani@siqes.com>
2024-04-05 15:38:29 +0530

    Dashboard font

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,9 +10,8 @@
                             <div class="position-relative"></div>
                             <div class="card-body pt-3 p-4">
                                 <h6 class="fw-semibold fs-4">Total PO</h6>
-                                <h3> </h3>
+                                <h3> {{$total_order_count}}</h3>
                                 <div class="d-flex align-items-center justify-content-between">
-{{$total_order_count}}
                                 </div>
                             </div>
                         </div>
@@ -22,22 +21,20 @@
                             <div class="position-relative"></div>
                             <div class="card-body pt-3 p-4">
                                 <h6 class="fw-semibold fs-4">Pending PO</h6>
-                                <h3>  </h3>
+                                <h3>{{$pending_order_count}}</h3>
                                 <div class="d-flex align-items-center justify-content-between">
-                                    {{$pending_order_count}}
 
                                 </div>
                             </div>
                         </div>
                     </div>
- <div class="col-sm-6 col-xl-3">
+                    <div class="col-sm-6 col-xl-3">
                         <div class="card overflow-hidden rounded-2">
                             <div class="position-relative"></div>
                             <div class="card-body pt-3 p-4">
                                 <h6 class="fw-semibold fs-4">Rejected PO</h6>
-                                <h3>  </h3>
+                                <h3>{{$rejected_order_count}}</h3>
                                 <div class="d-flex align-items-center justify-content-between">
-                                    {{$rejected_order_count}}
 
                                 </div>
                             </div>
@@ -49,9 +46,8 @@
                             <div class="position-relative"></div>
                             <div class="card-body pt-3 p-4">
                                 <h6 class="fw-semibold fs-4">Accepted PO</h6>
-                                <h3>  </h3>
+                                <h3>{{$accepted_order_count}}</h3>
                                 <div class="d-flex align-items-center justify-content-between">
-                                    {{$accepted_order_count}}
 
                                 </div>
                             </div>
@@ -62,9 +58,8 @@
                             <div class="position-relative"></div>
                             <div class="card-body pt-3 p-4">
                                 <h6 class="fw-semibold fs-4">Resubmitted PO</h6>
-                                <h3> </h3>
+                                <h3>{{$resubmitted_order_count}} </h3>
                                 <div class="d-flex align-items-center justify-content-between">
-                                    {{$resubmitted_order_count}}
                                 </div>
                             </div>
                         </div>
